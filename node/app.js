3e4ca4d99ac75de79d2fb122f9c310858bce936b
David Munguía Acosta <a214201827@unison.mx>
2024-11-05 15:00:13 -0700

    Intento de login funcional.

--- node/app.js
+++ node/app.js
@@ -7,6 +7,21 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
+// CORS con credenciales-
+
+/* 
+
+const cors = require('cors');
+app.use(cors({
+    origin: 'http://localhost', // Cambia al dominio de tu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+*/
+
+
+
 // Rutas
 const usuariosRoutes = require('./routes/usuarios');
 const backupRoutes = require('./routes/backup');
