89faefde8aeb69e4fc1875f9499bff4f8583ee90
David Munguía Acosta <a214201827@unison.mx>
2024-11-06 14:36:28 -0700

    Ajustes a la BD.

--- db/mariadb-image-docker-init.sql
+++ db/mariadb-image-docker-init.sql
@@ -161,35 +161,36 @@ DELIMITER ;
 
 
 -- Insertar datos en la tabla `courses`
-INSERT INTO courses (courseName, googleClassroomData) VALUES
+/*INSERT INTO courses (courseName, googleClassroomData) VALUES
 ('Mathematics 101', '{"classroomId": "abc123", "teacher": "Mr. Smith"}'),
 ('Physics 101', '{"classroomId": "def456", "teacher": "Dr. Brown"}'),
 ('Chemistry 101', '{"classroomId": "ghi789", "teacher": "Ms. Johnson"}'),
 ('Biology 101', '{"classroomId": "jkl012", "teacher": "Mr. Doe"}'),
 ('History 101', '{"classroomId": "mno345", "teacher": "Mr. Adams"}'),
 ('Literature 101', '{"classroomId": "pqr678", "teacher": "Ms. King"}'),
-('Computer Science 101', '{"classroomId": "stu901", "teacher": "Ms. Hall"}');
+('Computer Science 101', '{"classroomId": "stu901", "teacher": "Ms. Hall"}');*/
 
 -- Insertar datos en la tabla `students`
-INSERT INTO students (names, lastNames, email, googleUserId) VALUES
+/*INSERT INTO students (names, lastNames, email, googleUserId) VALUES
 ('John', 'Doe', 'john.doe@example.com', 'g123456'),
 ('Jane', 'Doe', 'jane.doe@example.com', 'g6dock54321'),
 ('Mike', 'Ross', 'mike.ross@example.com', 'g789123'),
 ('Rachel', 'Zane', 'rachel.zane@example.com', 'g321987'),
 ('Louis', 'Litt', 'louis.litt@example.com', 'g987654'),
 ('Harvey', 'Specter', 'harvey.specter@example.com', 'g852369'),
-('Donna', 'Paulsen', 'donna.paulsen@example.com', 'g456789');
+('Donna', 'Paulsen', 'donna.paulsen@example.com', 'g456789');*/
 
 -- Insertar datos en la tabla `usuarios`
-INSERT INTO usuarios (names, lastName, email, password_hash, status, userType) VALUES
+/*INSERT INTO usuarios (names, lastName, email, password_hash, status, userType) VALUES
 ('Admin', 'User', 'admin@example.com', 'password_hash_admin', 'Activo', 'Administrador'),
 ('Coord', 'User', 'coord@example.com', 'password_hash_coord', 'Inactivo', 'Coordinador'),
 ('Prof', 'Smith', 'smith@example.com', 'password_hash_prof', 'Pendiente', 'Profesor'),
 ('Prof', 'Brown', 'brown@example.com', 'password_hash_brown', 'Pendiente', 'Profesor'),
 ('Prof', 'Johnson', 'johnson@example.com', 'password_hash_johnson', 'Activo', 'Profesor'),
-('Prof', 'King', 'king@example.com', 'password_hash_king', 'Activo', 'Profesor');
+('Prof', 'King', 'king@example.com', 'password_hash_king', 'Activo', 'Profesor');*/
+
 -- Insertar datos en la tabla `studentCourses`
-INSERT INTO studentCourses (studentId, courseId, googleClassroomData) VALUES
+/*INSERT INTO studentCourses (studentId, courseId, googleClassroomData) VALUES
 (1, 1, '{"classroomId": "abc123", "grade": 95}'),
 (1, 2, '{"classroomId": "def456", "grade": 89}'),
 (2, 3, '{"classroomId": "ghi789", "grade": 76}'),
@@ -197,34 +198,34 @@ INSERT INTO studentCourses (studentId, courseId, googleClassroomData) VALUES
 (3, 1, '{"classroomId": "abc123", "grade": 85}'),
 (3, 5, '{"classroomId": "mno345", "grade": 92}'),
 (4, 6, '{"classroomId": "pqr678", "grade": 80}'),
-(5, 7, '{"classroomId": "stu901", "grade": 85}');
+(5, 7, '{"classroomId": "stu901", "grade": 85}');*/
 
 -- Insertar datos en la tabla `sessions`
-INSERT INTO sessions (dateStart, dateExpiration, token, userIpAddress, userAgent, usuario) VALUES
+/*INSERT INTO sessions (dateStart, dateExpiration, token, userIpAddress, userAgent, usuario) VALUES
 (NOW(), NOW() + INTERVAL 1 HOUR, 'token1', '192.168.0.1', 'Mozilla/5.0', 1),
 (NOW(), NOW() + INTERVAL 2 HOUR, 'token2', '192.168.0.2', 'Mozilla/5.0', 2),
 (NOW(), NOW() + INTERVAL 3 HOUR, 'token3', '192.168.0.3', 'Mozilla/5.0', 3),
 (NOW(), NOW() + INTERVAL 1 HOUR, 'token4', '192.168.0.4', 'Mozilla/5.0', 4),
 (NOW(), NOW() + INTERVAL 2 HOUR, 'token5', '192.168.0.5', 'Mozilla/5.0', 5),
-(NOW(), NOW() + INTERVAL 3 HOUR, 'token6', '192.168.0.6', 'Mozilla/5.0', 6);
+(NOW(), NOW() + INTERVAL 3 HOUR, 'token6', '192.168.0.6', 'Mozilla/5.0', 6);*/
 
 -- Insertar datos en la tabla `userCourses`
-INSERT INTO userCourses (course, user) VALUES
+/*INSERT INTO userCourses (course, user) VALUES
 (1, 3),
 (2, 4),
 (3, 5),
 (4, 6),
 (5, 3),
 (6, 4),
-(7, 5);
+(7, 5);*/
 
 -- Insertar datos en la tabla `registroAuditoria`
-INSERT INTO registroAuditoria (tipoMovimiento, nombreUsuario, usuarioMovimiento, usuarioQueRealizoAccion, fechaMovimiento) VALUES
+/*INSERT INTO registroAuditoria (tipoMovimiento, nombreUsuario, usuarioMovimiento, usuarioQueRealizoAccion, fechaMovimiento) VALUES
 ('UPDATE', 'Admin User', 'admin@example.com', 'admin@example.com', NOW()),
 ('UPDATE', 'Coord User', 'coord@example.com', 'admin@example.com', NOW()),
 ('DELETE', 'Prof Smith', 'smith@example.com', 'coord@example.com', NOW()),
 ('DELETE', 'Prof Brown', 'brown@example.com', 'coord@example.com', NOW()),
-('UPDATE', 'Prof King', 'king@example.com', 'admin@example.com', NOW());
+('UPDATE', 'Prof King', 'king@example.com', 'admin@example.com', NOW());*/
 
 
 /*insert into courses (courseName, googleClassroomData) values ('a ipsum integer a', '[{},{}]');
