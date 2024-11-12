246d8f6ad4d2377e92403c664ff6b832dfaec3d7
David Munguía Acosta <a214201827@unison.mx>
2024-11-11 23:41:09 -0700

    Vuelta a la orquestacion anterior.

--- db/healthcheck.sh
+++ db/healthcheck.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if mysqladmin ping -h localhost > /dev/null 2>&1; then
+if mysqladmin ping -h db > /dev/null 2>&1; then
   if mysql -u"$MARIADB_USER" -p"$MARIADB_PASSWORD" -e "USE $MARIADB_DATABASE; SELECT 1 FROM usuarios LIMIT 1;" > /dev/null 2>&1; then
     exit 0
   fi
